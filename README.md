e0251f82648b928bfd67c7aba3ef78e032e23b1f
jgarnet <jgarnett18@gmail.com>
2025-06-11 09:23:04 -0400

    push up changes

---
+++ README.md
@@ -0,0 +1,49 @@
+# order-ingestion API
+
+## Overview
+
+Basic batch API which ingests customer orders and inserts into a database.
+
+This project serves as a sandbox to explore different tools and approaches for processing batch payloads.
+
+## Architecture
+
+## Domain Model Layer
+
+The project uses [POJOs](./src/main/java/org/myshop/order) to represent the domain model and encapsulate validation logic.
+
+## Dependency Injection Layer
+
+The project is using Dagger 2 for dependency injection, leveraging a global [App Module](./src/main/java/org/myshop/di/AppModule.java) and [App Component](./src/main/java/org/myshop/di/AppComponent.java).
+
+## HTTP Layer
+
+The http layer uses a simple Java [HTTP Server implementation](./src/main/java/org/myshop/http/OrdersHttpServer.java) as a front controller for handling HTTP requests. Request handlers are split across multiple threads using the ExecutorService.
+
+Operations include [batch order ingestion](./src/main/java/org/myshop/http/BatchOrdersHandler.java) and [error store operations](./src/main/java/org/myshop/http/ErrorStoreHandler.java).
+
+## Batch Processing Layer
+
+The batch layer provides a [batch service](./src/main/java/org/myshop/batch/BatchOrdersService.java) which delegates processing to several [processors](./src/main/java/org/myshop/batch/BatchOrdersProcessor.java) split across multiple threads using the ExecutorService.
+
+## Queue Layer
+
+The queue layer provides [basic queue operations](./src/main/java/org/myshop/queue/QueueProvider.java) and an [error store](./src/main/java/org/myshop/queue/ErrorStore.java) for batches which have exceeded the retry threshold.
+
+## Persistence Layer
+
+The persistence layer provides [database](./src/main/java/org/myshop/persistence/database/Database.java) access, leveraging [HikariCP](./src/main/java/org/myshop/persistence/database/HikariDatabase.java) for connection pooling.
+
+The [repository](./src/main/java/org/myshop/persistence/repository) layer provides a simple native SQL implementation for data source interactions.
+
+## Running Locally
+
+The project leverages [Docker](./Dockerfile) and [docker-compose](./local/docker-compose.yml) for local use.
+
+The [scripts](./scripts) directory provides scripts for quickly standing up and tearing down the Docker resources:
+- [docker-run.sh](./scripts/docker-run.sh) builds and runs the containers in docker-compose.yml
+- [docker-down.sh](./scripts/docker-down.sh) tears down the containers
+
+## Submitting Payloads
+
+The [client](./client) subproject provides a simple NodeJS implementation that creates sample order batches and submits them to the API.
--- local/db/seed.sql
+++ local/db/seed.sql
@@ -1,35 +1,65 @@
 -- DDL
-CREATE TABLE products (
-    id VARCHAR(255) NOT NULL PRIMARY KEY,
+CREATE TABLE products
+(
+    id           VARCHAR(255) NOT NULL PRIMARY KEY,
     product_name VARCHAR(255) NOT NULL,
-    price DECIMAL NOT NULL
+    price        DECIMAL      NOT NULL
 );
 
-CREATE TABLE orders (
-    id BINARY(16) PRIMARY KEY,
+CREATE TABLE orders
+(
+    id         BINARY(16) PRIMARY KEY,
     order_date TIMESTAMP
 );
 
-CREATE TABLE order_customers (
-    order_id BINARY(16) PRIMARY KEY,
+CREATE TABLE order_customers
+(
+    order_id   BINARY(16) PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255),
-    phone VARCHAR(10)
+    last_name  VARCHAR(255) NOT NULL,
+    email      VARCHAR(255),
+    phone      VARCHAR(10)
 );
-ALTER TABLE order_customers ADD FOREIGN KEY (order_id) REFERENCES orders(id);
+ALTER TABLE order_customers
+    ADD FOREIGN KEY (order_id) REFERENCES orders (id);
 
-CREATE TABLE order_lines (
-    order_id BINARY(16) NOT NULL,
+CREATE TABLE order_lines
+(
+    order_id    BINARY(16) NOT NULL,
     line_number INT,
-    product_id VARCHAR(255) NOT NULL,
-    quantity INT
+    product_id  VARCHAR(255) NOT NULL,
+    quantity    INT
 );
-ALTER TABLE order_lines ADD PRIMARY KEY (order_id, line_number);
-ALTER TABLE order_lines ADD FOREIGN KEY (order_id) REFERENCES orders(id);
-ALTER TABLE order_lines ADD FOREIGN KEY (product_id) REFERENCES products(id);
+ALTER TABLE order_lines
+    ADD PRIMARY KEY (order_id, line_number);
+ALTER TABLE order_lines
+    ADD FOREIGN KEY (order_id) REFERENCES orders (id);
+ALTER TABLE order_lines
+    ADD FOREIGN KEY (product_id) REFERENCES products (id);
 
 -- DML
 -- Products
-INSERT INTO products VALUES ('iphone11', 'iPhone 11', 500.00);
+INSERT INTO products
+VALUES ('iphone11', 'iPhone 11', 500.00);
 commit;
+
+-- Views
+CREATE VIEW orders_summary AS
+select bin_to_uuid(o.id)                      "Order ID",
+       o.order_date                           "Order Date",
+       CONCAT(c.first_name, ' ', c.last_name) "Customer Name",
+       CONCAT('$',
+              FORMAT(
+                      SUM((select l.quantity * p.price
+                           from order_lines l
+                                    join products p
+                                         on l.product_id = p.id
+                           where l.order_id = o.id)
+                          ), 2
+                  )
+           )                                  "Order Cost"
+from orders o
+         join order_customers c
+              on o.id = c.order_id
+group by o.id
+order by o.order_date asc;
